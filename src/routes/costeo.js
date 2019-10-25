6901077ed06740d1b4730a11fa870872038d6d54
dennisrmz <orellanadennis12@gmail.com>
2019-10-25 01:38:31 -0600

    Agregando vistas de costeo

--- src/routes/costeo.js
+++ src/routes/costeo.js
@@ -3,6 +3,22 @@ const router = express.Router();
 
 const pool = require('../database');
 
+router.get('/', async (req, res) => {
+    res.render('costeo/listar_saldos_PT');
+    });
+
+router.get('/producto_proceso', async (req, res) => {
+    res.render('costeo/productos_procesos');
+    });
+
+router.get('/detalle_producto_proceso', async (req, res) => {
+    res.render('costeo/detalle_producto_proceso');
+    });
+
+router.get('/transferir_proceso',(req, res) => {
+    res.render('costeo/form_cambiar_proceso_producto');
+});
+
 router.get('/agregar', (req, res) => {
     res.render('costeo/agregar_entradas_PT');
 })
