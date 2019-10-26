c0b3b132beb5f5ac9bdc72e8fd6053c3b83bcdfd
dennisrmz <orellanadennis12@gmail.com>
2019-10-25 22:10:31 -0600

    Vistas costeo avance 2

--- src/routes/costeo.js
+++ src/routes/costeo.js
@@ -3,13 +3,42 @@ const router = express.Router();
 
 const pool = require('../database');
 
-router.get('/', async (req, res) => {
-    res.render('costeo/listar_saldos_PT');
+//*******************Rutas Materia Prima******************* */ 
+router.get('/materia_prima_saldo', async (req, res) => {
+    res.render('materia-prima/listar_saldos_materia_prima');
     });
+
+router.get('/materia_prima_entrada', async (req, res) => {
+    res.render('materia-prima/listar_entradas_materia_prima');
+    });
+
+router.get('/materia_prima_salida', async (req, res) => {
+    res.render('materia-prima/listar_salidas_materia_prima');
+    });
+
+
+//*******************Rutas Producto Terminado***************** */
+router.get('/producto_terminado_saldo', async (req, res) => {
+    res.render('costeo/listar_saldos_producto_terminado');
+    });
+
+router.get('/producto_terminado_entrada', async (req, res) => {
+    res.render('costeo/listar_entradas_producto_terminado');
+    });
+
+router.get('/producto_terminado_salida', async (req, res) => {
+    res.render('costeo/listar_salidas_producto_terminado');
+    });
+
+//*******************Rutas Producto en Proceso***************** */  
 
 router.get('/producto_proceso', async (req, res) => {
     res.render('costeo/productos_procesos');
     });
+    
+router.get('/agregar_productos_proceso', (req, res) => {
+    res.render('costeo/agregar_productos_a_proceso');
+    })
 
 router.get('/detalle_producto_proceso', async (req, res) => {
     res.render('costeo/detalle_producto_proceso');
@@ -19,7 +48,5 @@ router.get('/transferir_proceso',(req, res) => {
     res.render('costeo/form_cambiar_proceso_producto');
 });
 
-router.get('/agregar', (req, res) => {
-    res.render('costeo/agregar_entradas_PT');
-})
+
 module.exports = router;
