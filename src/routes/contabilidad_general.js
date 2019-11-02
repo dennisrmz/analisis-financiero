f05666e075b6c33cf32cd434b618f4e6ee070f64
RosaAlfaro <rosalfaro96@gmail.com>
2019-11-01 19:30:53 -0600

    Rutas del estado financiero

--- src/routes/contabilidad_general.js
+++ src/routes/contabilidad_general.js
@@ -3,46 +3,83 @@ const router = express.Router();
 
 const pool = require('../database')
 
-//Listado Estados Financieros
+//-----------------------------------BALANCE DE COMPROBACION---------------------------------------------------------
+router.get('/estados_financieros/balance_comprobacion/:ID_ESTADOFINANCIERO', async (req, res) => {
+    const { ID_ESTADOFINANCIERO } = req.params;
+    const estadofinanciero = await pool.query('SELECT * FROM estadofinanciero WHERE ID_ESTADOFINANCIERO = ?', [ ID_ESTADOFINANCIERO ]);
+    res.render('contabilidad_general/balance_de_comprobacion', {estadofinanciero});
+});
+
+//---------------------------------------BALANCE GENERAL---------------------------------------------------------
+router.get('/estados_financieros/balance_general/:ID_ESTADOFINANCIERO', async (req, res) => {
+    const { ID_ESTADOFINANCIERO } = req.params;
+    const estadofinanciero = await pool.query('SELECT * FROM estadofinanciero WHERE ID_ESTADOFINANCIERO = ?', [ ID_ESTADOFINANCIERO ]);
+    res.render('contabilidad_general/balance_general', {estadofinanciero});
+});
+
+//--------------------------------------ESTADO DE RESULTADOS---------------------------------------------------------
+router.get('/estados_financieros/estado_resultados/:ID_ESTADOFINANCIERO', async (req, res) => {
+    const { ID_ESTADOFINANCIERO } = req.params;
+    const estadofinanciero = await pool.query('SELECT * FROM estadofinanciero WHERE ID_ESTADOFINANCIERO = ?', [ ID_ESTADOFINANCIERO ]);
+    res.render('contabilidad_general/estado_de_resultados', {estadofinanciero});
+});
+
+//----------------------------------------ESTADO DE CAPITAL---------------------------------------------------------
+router.get('/estados_financieros/estado_capital/:ID_ESTADOFINANCIERO', async (req, res) => {
+    const { ID_ESTADOFINANCIERO } = req.params;
+    const estadofinanciero = await pool.query('SELECT * FROM estadofinanciero WHERE ID_ESTADOFINANCIERO = ?', [ ID_ESTADOFINANCIERO ]);
+    res.render('contabilidad_general/estado_de_capital', {estadofinanciero});
+});
+
+//-------------------------------------ESTADO DE FLUJO DE EFECTIVO--------------------------------------------------
+router.get('/estados_financieros/estado_flujo_efectivo/:ID_ESTADOFINANCIERO', async (req, res) => {
+    const { ID_ESTADOFINANCIERO } = req.params;
+    const estadofinanciero = await pool.query('SELECT * FROM estadofinanciero WHERE ID_ESTADOFINANCIERO = ?', [ ID_ESTADOFINANCIERO ]);
+    res.render('contabilidad_general/estado_flujo_de_efectivo', {estadofinanciero});
+});
+
+//------------------------------------------NOTAS EXPLICATIVAS------------------------------------------------------
+
+//Mostrar listado de notas explicativas del estado financiero elegido
+router.get('/estados_financieros/:NOMBRE_ESTADOFINANCIERO/:ID_ESTADOFINANCIERO/notas_explicativas/listado',async (req, res,next) => {
+    const { NOMBRE_ESTADOFINANCIERO } = req.params;
+    const { ID_ESTADOFINANCIERO } = req.params;
+    const notasexplicativas = pool.query('SELECT * FROM notaexplicativa INNER JOIN estadofinanciero ON ' +
+    'notaexplicativa.ID_ESTADOFINANCIERO = estadofinanciero.ID_ESTADOFINANCIERO WHERE estadofinanciero.ID_ESTADOFINANCIERO = ?', [ ID_ESTADOFINANCIERO ]);
+    console.log("ID_ESTADOFINANCIERO");
+    console.log({ID_ESTADOFINANCIERO});
+    res.render('contabilidad_general/listado_notas_explicativas', {notasexplicativas, ID_ESTADOFINANCIERO, NOMBRE_ESTADOFINANCIERO});
+});
+
+//Agregar Notas Explicativas
+router.get('/estados_financieros/:NOMBRE_ESTADOFINANCIERO/:ID_ESTADOFINANCIERO/notas_explicativas/listado/agregar', (req, res) => {
+    const { ID_ESTADOFINANCIERO } = req.params;
+    console.log({ID_ESTADOFINANCIERO});
+    res.render('contabilidad_general/agregar');
+});
+router.post('/estados_financieros/agregar_nota_explicativa', (req, res) => {
+    console.log(req.body);
+    res.send('recibido');
+});
+
+//---------------------------------------ESTADOS FINANCIEROS---------------------------------------------------------
+
+//Mostrar listado de estados financieros
 router.get('/estados_financieros/listado', async (req, res) => {
     const estadofinanciero = await pool.query('SELECT * FROM estadofinanciero');
     res.render('contabilidad_general/listado_estados_financieros', {estadofinanciero});
 });
 
-//Balance de Comprobacion
-router.get('/estados_financieros/balance_comprobacion', async (req, res) => {
-    res.render('contabilidad_general/balance_de_comprobacion');
-});
-
-//Balance General
-router.get('/estados_financieros/balance_general', async (req, res) => {
-    res.render('contabilidad_general/balance_general');
-});
-
-//Estado de Resultados
-router.get('/estados_financieros/estado_resultados', async (req, res) => {
-    res.render('contabilidad_general/estado_de_resultados');
-});
-
-//Estado de Capital
-router.get('/estados_financieros/estado_capital', async (req, res) => {
-    res.render('contabilidad_general/estado_de_capital');
-});
-
-//Estado de Flujo de Efectivo
-router.get('/estados_financieros/estado_flujo_efectivo', async (req, res) => {
-    res.render('contabilidad_general/estado_flujo_de_efectivo');
-});
-
-//Notas Explicativas (agregar)
-router.get('/estados_financieros/agregar_nota_explicativa', async (req, res) => {
-    res.render('contabilidad_general/agregar_nota_explicativa');
-});
-
-//Listado de Notas Explicativas 
-router.get('/estados_financieros/notas_explicativas/listado', async (req, res) => {
-    res.render('contabilidad_general/listado_notas_explicativas');
+//Mostrar el estado financiero elegido
+router.get('/estados_financieros/:NOMBRE_ESTADOFINANCIERO/:ID_ESTADOFINANCIERO', async (req, res) => {
+    const { NOMBRE_ESTADOFINANCIERO } = req.params;
+    const { ID_ESTADOFINANCIERO } = req.params;
+    const estadofinanciero = await pool.query('SELECT * FROM estadofinanciero WHERE ID_ESTADOFINANCIERO = ? AND ' +
+    'NOMBRE_ESTADOFINANCIERO = ?', [ ID_ESTADOFINANCIERO, NOMBRE_ESTADOFINANCIERO ]);
+    res.render('contabilidad_general/listado_estados_financieros', {estadofinanciero});
 });
 
 module.exports = router;
+
+
 
