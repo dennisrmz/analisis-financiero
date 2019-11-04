3f4ea7d2975eb836e96212b0ff5eec3e4f587784
RosaAlfaro <rosalfaro96@gmail.com>
2019-11-04 13:55:59 -0600

    Ver cuentas en balance

--- database/db.sql
+++ database/db.sql
@@ -14,9 +14,9 @@ drop table if exists MOVIMIENTO;
 
 drop table if exists NATURALEZA;
 
-drop table if exists NOTAEXPLICATIVA;
-
 drop table if exists PERIODOCONTABLE;
+
+drop table if exists TIPOAJUSTE;
 
 drop table if exists TIPOTRANSACCION;
 
@@ -27,7 +27,7 @@ drop table if exists TRANSACCION;
 /*==============================================================*/
 create table CUENTA
 (
-   ID_CUENTA            int not null,
+   ID_CUENTA            int not null AUTO_INCREMENT,
    ID_NATURALEZA_CUENTA int,
    CODIGO_CUENTA_PADRE  int,
    CODIGO_CUENTA        int not null,
@@ -41,7 +41,7 @@ create table CUENTA
 /*==============================================================*/
 create table ESTADOFINANCIERO
 (
-   ID_ESTADOFINANCIERO  int not null,
+   ID_ESTADOFINANCIERO  int not null AUTO_INCREMENT,
    ID_PERIODOCONTABLE   int not null,
    NOMBRE_ESTADOFINANCIERO varchar(45) not null,
    primary key (ID_ESTADOFINANCIERO)
@@ -62,9 +62,10 @@ create table ESTADOFINANCIERO_MAYORIZACION
 /*==============================================================*/
 create table MAYORIZACION
 (
-   ID_MAYORIZACION      int not null,
+   ID_MAYORIZACION      int not null AUTO_INCREMENT,
+   ID_CUENTA            int,
    MONTO_SALDO          decimal not null,
-   ES_SALDO_ACREEDOR    bool not null,
+   ES_SALDO_ACREEDOR    boolean not null,
    primary key (ID_MAYORIZACION)
 );
 
@@ -73,10 +74,11 @@ create table MAYORIZACION
 /*==============================================================*/
 create table MOVIMIENTO
 (
-   ID_MOVIMIENTO        int not null,
+   ID_MOVIMIENTO        int not null AUTO_INCREMENT,
    ID_TRANSACCION       int,
-   ID_MAYORIZACION      int,
    ID_CUENTA            int,
+   FECHA_MOVIMIENTO     date not null,
+   DETALLE_MOVIMIENTO   varchar(250) not null,
    MONTO_CARGO          decimal not null,
    MONTO_ABONO          decimal not null,
    primary key (ID_MOVIMIENTO)
@@ -87,21 +89,9 @@ create table MOVIMIENTO
 /*==============================================================*/
 create table NATURALEZA
 (
-   ID_NATURALEZA_CUENTA int not null,
+   ID_NATURALEZA_CUENTA int not null AUTO_INCREMENT,
    TIPO_NATURALEZA_CUETA varchar(10) not null,
    primary key (ID_NATURALEZA_CUENTA)
-);
-
-/*==============================================================*/
-/* Table: NOTAEXPLICATIVA                                       */
-/*==============================================================*/
-create table NOTAEXPLICATIVA
-(
-   ID_NOTA              int not null,
-   ID_ESTADOFINANCIERO  int,
-   TITULO_NOTA          varchar(50) not null,
-   DESCRIPCION_NOTA     varchar(300) not null,
-   primary key (ID_NOTA)
 );
 
 /*==============================================================*/
@@ -109,10 +99,21 @@ create table NOTAEXPLICATIVA
 /*==============================================================*/
 create table PERIODOCONTABLE
 (
-   ID_PERIODOCONTABLE   int not null,
+   ID_PERIODOCONTABLE   int not null AUTO_INCREMENT,
    FECHAINICIO_PERIODO  date not null,
    FECHAFINAL_PERIODO   date,
    primary key (ID_PERIODOCONTABLE)
+);
+
+/*==============================================================*/
+/* Table: TIPOAJUSTE                                            */
+/*==============================================================*/
+create table TIPOAJUSTE
+(
+   CODIGO_TIPO_AJUSTE   int not null AUTO_INCREMENT,
+   NOMBRE_TIPO_AJUSTE   varchar(50) not null,
+   DESCRIPCION_TIPO_AJUSTE varchar(250) not null,
+   primary key (CODIGO_TIPO_AJUSTE)
 );
 
 /*==============================================================*/
@@ -120,7 +121,7 @@ create table PERIODOCONTABLE
 /*==============================================================*/
 create table TIPOTRANSACCION
 (
-   CODIGO_TIPO_TRANSACCION int not null,
+   CODIGO_TIPO_TRANSACCION int not null AUTO_INCREMENT,
    NOMBRE_TIPO_TRANSACCION char(50) not null,
    DESCRIPCION_TIPO_TRANSACCION char(250) not null,
    primary key (CODIGO_TIPO_TRANSACCION)
@@ -131,45 +132,43 @@ create table TIPOTRANSACCION
 /*==============================================================*/
 create table TRANSACCION
 (
-   ID_TRANSACCION       int not null,
+   ID_TRANSACCION       int not null AUTO_INCREMENT,
    CODIGO_TIPO_TRANSACCION int,
    ID_PERIODOCONTABLE   int,
-   CODIGO_TRANSACCION   int not null,
+   CODIGO_TIPO_AJUSTE   int,
+   DESCRIPCION_TRANSACCION varchar(250) not null,
    FECHA_TRANSACCION    date not null,
-   NUMERO_TRANSACCION   int,
-   DESCRIPCION_TRANSACCION varchar(50) not null,
    MONTO_TRANSACCION    decimal not null,
-   ES_AJUSTE            bool not null,
+   ES_AJUSTE            boolean not null,
    primary key (ID_TRANSACCION)
 );
 
 alter table CUENTA add constraint FK_POSEE1 foreign key (ID_NATURALEZA_CUENTA)
       references NATURALEZA (ID_NATURALEZA_CUENTA) on delete restrict on update restrict;
 
-alter table ESTADOFINANCIERO add constraint FK_ESTADOFINANCIERO_PERIODOCONTABLE foreign key (ID_PERIODOCONTABLE)
+alter table ESTADOFINANCIERO add constraint FK_PERTENECE_A foreign key (ID_PERIODOCONTABLE)
       references PERIODOCONTABLE (ID_PERIODOCONTABLE) on delete restrict on update restrict;
 
-alter table ESTADOFINANCIERO_MAYORIZACION add constraint FK_ESTADOFINANCIERO_MAYORIZACION foreign key (ID_ESTADOFINANCIERO)
+alter table ESTADOFINANCIERO_MAYORIZACION add constraint FK_ESTADOFINANCIERO_ESTADOFINANCIERO_MAYORIZACION foreign key (ID_ESTADOFINANCIERO)
       references ESTADOFINANCIERO (ID_ESTADOFINANCIERO) on delete restrict on update restrict;
 
 alter table ESTADOFINANCIERO_MAYORIZACION add constraint FK_ESTADOFINANCIERO_MAYORIZACION2 foreign key (ID_MAYORIZACION)
       references MAYORIZACION (ID_MAYORIZACION) on delete restrict on update restrict;
 
-alter table MOVIMIENTO add constraint FK_CUENTA_MOVIMIENTO foreign key (ID_CUENTA)
+alter table MAYORIZACION add constraint FK_TIENE foreign key (ID_CUENTA)
       references CUENTA (ID_CUENTA) on delete restrict on update restrict;
-
-alter table MOVIMIENTO add constraint FK_MAYORIZACION_MOVIMIENTO foreign key (ID_MAYORIZACION)
-      references MAYORIZACION (ID_MAYORIZACION) on delete restrict on update restrict;
 
 alter table MOVIMIENTO add constraint FK_OCURREN foreign key (ID_TRANSACCION)
       references TRANSACCION (ID_TRANSACCION) on delete restrict on update restrict;
 
-alter table NOTAEXPLICATIVA add constraint FK_TIENE foreign key (ID_ESTADOFINANCIERO)
-      references ESTADOFINANCIERO (ID_ESTADOFINANCIERO) on delete restrict on update restrict;
-
-alter table TRANSACCION add constraint FK_PERIODOCONTABLE_TRANSACCION foreign key (ID_PERIODOCONTABLE)
-      references PERIODOCONTABLE (ID_PERIODOCONTABLE) on delete restrict on update restrict;
+alter table MOVIMIENTO add constraint FK_REALIZA foreign key (ID_CUENTA)
+      references CUENTA (ID_CUENTA) on delete restrict on update restrict;
 
 alter table TRANSACCION add constraint FK_POSEE foreign key (CODIGO_TIPO_TRANSACCION)
       references TIPOTRANSACCION (CODIGO_TIPO_TRANSACCION) on delete restrict on update restrict;
 
+alter table TRANSACCION add constraint FK_POSEE_3 foreign key (CODIGO_TIPO_AJUSTE)
+      references TIPOAJUSTE (CODIGO_TIPO_AJUSTE) on delete restrict on update restrict;
+
+alter table TRANSACCION add constraint FK_REQUIERE foreign key (ID_PERIODOCONTABLE)
+      references PERIODOCONTABLE (ID_PERIODOCONTABLE) on delete restrict on update restrict;
