968e55a1191b0f554312da50870d483c1e9c63cb
EliMendez <elixa_mendex@hotmail.com>
2019-11-14 00:38:51 -0600

    Agregando vistas de authentication

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,4 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const pool = require('../database');
+
+// SINGIN
+router.get('/signin', (req, res) => {
+        res.render('authentication/signin');
+});
+router.post('/signin', (req, res) =>{
+        res.redirect('/');
+}); 
+
+// SIGNUP
+router.get('/signup', (req, res) => {
+        res.render('authentication/signup');
+});
+router.post('/signup', (req, res) =>{
+        res.redirect('/signin');
+}); 
+
 module.exports = router;
