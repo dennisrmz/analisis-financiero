c950189c7652adeef20f463d2443a524d72f0525
RosaAlfaro <rosalfaro96@gmail.com>
2019-11-20 18:01:04 -0600

    Modificaciones en las vistas de los EF

--- src/routes/contabilidad_general.js
+++ src/routes/contabilidad_general.js
@@ -59,11 +59,14 @@ router.get('/estados_financieros/BALANCE_DE_COMPROBACION_INICIAL/:ID_ESTADOFINAN
         const idioma = await pool.query('SET lc_time_names = "es_VE"');
         const periodoscontables = await pool.query('SELECT DATE_FORMAT(periodocontable.FECHAINICIO_PERIODO, "%d") AS FECHAINICIO, DATE_FORMAT(periodocontable.FECHAFINAL_PERIODO, "%d") AS FECHAFINAL, DATE_FORMAT(periodocontable.FECHAINICIO_PERIODO, "%M") AS MES, DATE_FORMAT(periodocontable.FECHAINICIO_PERIODO, "%Y") AS ANIO ' +
         'FROM periodocontable INNER JOIN estadofinanciero ON estadofinanciero.ID_PERIODOCONTABLE=periodocontable.ID_PERIODOCONTABLE WHERE ID_ESTADOFINANCIERO = ?', [ ID_ESTADOFINANCIERO ]);
-        const cuentas_patrimonio = await pool.query('SELECT * FROM cuenta WHERE cuenta.CODIGO_CUENTA LIKE "3%"');
+        const cuentas_patrimonio = await pool.query('SELECT * FROM cuenta WHERE cuenta.CODIGO_CUENTA = "311"');
         //const cuentas_activo = await(pool.query('SELECT cuenta.CODIGO_CUENTA, cuenta.NOMBRE_CUENTA, mayorizacion.MONTO_SALDO, mayorizacion.ES_SALDO_ACREEDOR FROM cuenta INNER JOIN mayorizacion ON cuenta.ID_CUENTA= mayorizacion. ID_CUENTA INNER JOIN estadofinanciero_mayorizacion ON mayorizacion.ID_MAYORIZACION= estadofinanciero_mayorizacion. ID_MAYORIZACION INNER JOIN estadofinanciero ON estadofinanciero.ID_ESTADOFINANCIERO= estadofinanciero_mayorizacion. ID_ESTADOFINANCIERO INNER JOIN periodocontable ON periodocontable.ID_PERIODOCONTABLE= estadofinanciero.ID_PERIODOCONTABLE WHERE estadofinanciero .ID_ESTADOFINANCIERO=? AND cuenta.CODIGO_CUENTA LIKE "1%"', [ID_ESTADOFINANCIERO]));
         //const cuentas_pasivo = await(pool.query('SELECT cuenta.CODIGO_CUENTA, cuenta.NOMBRE_CUENTA, mayorizacion.MONTO_SALDO, mayorizacion.ES_SALDO_ACREEDOR FROM cuenta INNER JOIN mayorizacion ON cuenta.ID_CUENTA= mayorizacion. ID_CUENTA INNER JOIN estadofinanciero_mayorizacion ON mayorizacion.ID_MAYORIZACION= estadofinanciero_mayorizacion. ID_MAYORIZACION INNER JOIN estadofinanciero ON estadofinanciero.ID_ESTADOFINANCIERO= estadofinanciero_mayorizacion. ID_ESTADOFINANCIERO INNER JOIN periodocontable ON periodocontable.ID_PERIODOCONTABLE= estadofinanciero.ID_PERIODOCONTABLE WHERE estadofinanciero .ID_ESTADOFINANCIERO=? AND cuenta.CODIGO_CUENTA LIKE "2%"', [ID_ESTADOFINANCIERO]));
         //const cuentas_patrimonio = await(pool.query('SELECT cuenta.CODIGO_CUENTA, cuenta.NOMBRE_CUENTA, mayorizacion.MONTO_SALDO, mayorizacion.ES_SALDO_ACREEDOR FROM cuenta INNER JOIN mayorizacion ON cuenta.ID_CUENTA= mayorizacion. ID_CUENTA INNER JOIN estadofinanciero_mayorizacion ON mayorizacion.ID_MAYORIZACION= estadofinanciero_mayorizacion. ID_MAYORIZACION INNER JOIN estadofinanciero ON estadofinanciero.ID_ESTADOFINANCIERO= estadofinanciero_mayorizacion. ID_ESTADOFINANCIERO INNER JOIN periodocontable ON periodocontable.ID_PERIODOCONTABLE= estadofinanciero.ID_PERIODOCONTABLE WHERE estadofinanciero .ID_ESTADOFINANCIERO=? AND cuenta.CODIGO_CUENTA LIKE "3%"', [ID_ESTADOFINANCIERO]));
-        res.render('contabilidad_general/estado_de_capital', {estadofinanciero, periodocontable:periodoscontables[0], idioma, cuentas_patrimonio});
+        var total = "";
+        total =cuentas_patrimonio[0].SALDO_CUENTA;
+        console.log("Total capital " +total);
+        res.render('contabilidad_general/estado_de_capital', {estadofinanciero, periodocontable:periodoscontables[0], idioma, cuentas_patrimonio, total});
     });
     
     //---------------------------------------BALANCE GENERAL---------------------------------------------------------
@@ -436,7 +439,7 @@ router.post('/transaccion/agregar_transaccion', async (req, res, next) => {
                 console.log('Fila insertada correctamente de movimiento:'+k);
                 //Actualizar el saldo de cada cuenta cuando se hace un movimiento
                 const naturaleza_cuenta = await pool.query('SELECT ID_NATURALEZA_CUENTA FROM cuenta WHERE ID_CUENTA=?', [new_movimiento.ID_CUENTA]);
-                const saldo_cuenta = await pool.query('SELECT SALDO_CUENTA FROM cuenta WHERE ID_CUENTA=? AND ID_NATURALEZA_CUENTA=1', [new_movimiento.ID_CUENTA]);
+                const saldo_cuenta = await pool.query('SELECT SALDO_CUENTA FROM cuenta WHERE ID_CUENTA=?', [new_movimiento.ID_CUENTA]);
                 var montoCargo = new_movimiento.MONTO_CARGO;
                 var montoAbono = new_movimiento.MONTO_ABONO;
                 var saldo_new = 0;
@@ -445,7 +448,7 @@ router.post('/transaccion/agregar_transaccion', async (req, res, next) => {
                         console.log("Nuevo saldo de una cuenta deudora: "+saldo_new);
                         await pool.query('UPDATE cuenta SET SALDO_CUENTA=? WHERE ID_CUENTA=?', [saldo_new, new_movimiento.ID_CUENTA] );
                 
-                }else{
+                }else if(naturaleza_cuenta[0].ID_NATURALEZA_CUENTA == 2){
                         saldo_new = saldo_cuenta[0].SALDO_CUENTA - montoCargo + montoAbono;
                         console.log("Nuevo saldo de una cuenta acreedora: "+saldo_new);
                         await pool.query('UPDATE cuenta SET SALDO_CUENTA=? WHERE ID_CUENTA=?', [saldo_new, new_movimiento.ID_CUENTA] );
