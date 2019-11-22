8b046af3be9eb2b42ba97c276ce33d32ce642e3b
Dennis Ramirez <51470416+dennisrmz@users.noreply.github.com>
2019-11-21 21:12:48 -0600

    Update contabilidad_general.js

--- src/routes/contabilidad_general.js
+++ src/routes/contabilidad_general.js
@@ -864,47 +864,6 @@ router.get('/monto_impuesto/:monto_tran', isLoggedIn,  async (req, res) => {
         res.render('contabilidad_general/monto_impuesto', {monto});
 });
 
-//Listado Estados Financieros
-router.get('/estados_financieros/listado', isLoggedIn, async (req, res) => {
-    const estadofinanciero = await pool.query('SELECT * FROM estadofinanciero');
-    res.render('contabilidad_general/listado_estados_financieros', {estadofinanciero});
-});
-
-//Balance de Comprobacion
-router.get('/estados_financieros/balance_comprobacion', isLoggedIn, async (req, res) => {
-    res.render('contabilidad_general/balance_de_comprobacion');
-});
-
-//Balance General
-router.get('/estados_financieros/balance_general', isLoggedIn, async (req, res) => {
-    res.render('contabilidad_general/balance_general');
-});
-
-//Estado de Resultados
-router.get('/estados_financieros/estado_resultados', isLoggedIn, async (req, res) => {
-    res.render('contabilidad_general/estado_de_resultados');
-});
-
-//Estado de Capital
-router.get('/estados_financieros/estado_capital', isLoggedIn, async (req, res) => {
-    res.render('contabilidad_general/estado_de_capital');
-});
-
-//Estado de Flujo de Efectivo
-router.get('/estados_financieros/estado_flujo_efectivo', isLoggedIn, async (req, res) => {
-    res.render('contabilidad_general/estado_flujo_de_efectivo');
-});
-
-//Notas Explicativas (agregar)
-router.get('/estados_financieros/agregar_nota_explicativa', isLoggedIn, async (req, res) => {
-    res.render('contabilidad_general/agregar_nota_explicativa');
-});
-
-//Listado de Notas Explicativas 
-router.get('/estados_financieros/notas_explicativas/listado', isLoggedIn, async (req, res) => {
-    res.render('contabilidad_general/listado_notas_explicativas');
-});
-
 router.get('/planilla', isLoggedIn,  async (req, res) => {
         const planilla_salarios=  await pool.query("SELECT * FROM planilla")
           res.render('contabilidad_general/planilla', {planilla_salarios});
