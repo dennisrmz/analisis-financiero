8fe82eb1162da12e9d1179d795e707fe1987f2cc
Dennis Ramirez <51470416+dennisrmz@users.noreply.github.com>
2019-11-22 01:37:18 -0600

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
 
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'analisisfinanciero'
+        host: 'b1gfaeyxl5cpvhcxhega-mysql.services.clever-cloud.com',
+        user: 'ux2vdr1f3twtt66s',
+        password: 'oXjIWfr1BiSM2ag4S7QI',
+        database: 'b1gfaeyxl5cpvhcxhega'
     }
 };
