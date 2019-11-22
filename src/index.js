d27d88b572b6fb3ff8e815608523905de188cab7
Dennis Ramirez <51470416+dennisrmz@users.noreply.github.com>
2019-11-22 01:24:09 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app = express();
 require('./lib/passport');
 
 // settings
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
